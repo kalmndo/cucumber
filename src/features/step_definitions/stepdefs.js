3fd8e900659f6089cd1e68fdee3bd61584e11ab1
adam <kalmndo@gmail.com>
2021-03-16 22:00:13 +0700

    first init

--- src/features/step_definitions/stepdefs.js
+++ src/features/step_definitions/stepdefs.js
@@ -1,24 +1,28 @@
 const { Given, Then } = require("@cucumber/cucumber");
-const { loginPage, setUsernamAndPassword, clickLogin } = require("../../page/login");
-const 
+const {
+  loginPage,
+  setUsernamAndPassword,
+  clickLogin,
+} = require("../../page/login");
 
 const {
   searchCaseByClaimUnique,
   transferCaseToRightUser,
 } = require("../../page/manageTask");
+const { viewDetailCase } = require("../../page/workBasket");
 
 const { caNum, selectCaseButton } = require("../../util/element");
 
 Given("I am on the pruhub login page", async function () {
-  await loginPage()
+  await loginPage();
 });
 
 Then("I set username and password", async function () {
-  await setUsernamAndPassword()
+  await setUsernamAndPassword();
 });
 
 Then("I click Sign In button", async function () {
-  await clickLogin()
+  await clickLogin();
 });
 
 Then("I go to Manage Task Claim menu", async function () {
@@ -39,4 +43,8 @@ Then("I transfer case to user", async function () {
 
 Then("go to workbasket", async function () {
   await goToWorkbasketMenu();
+});
+
+Then("go to the case and view detail", async function () {
+  await viewDetailCase();
 });
--- src/page/workBasket.js
+++ src/page/workBasket.js
@@ -1,3 +1,45 @@
+const { By } = require("selenium-webdriver");
+const { driver } = require("../util/driver");
+const { loadingWraper, caNum } = require("../util/element");
+const { waitInvisibility } = require("../util/helper");
 
+const checkWorkbasket = async (cuNumber) => {
+  await waitInvisibility(loadingWraper);
+  let cuNumberElms = By.xpath(
+    "//span[@class='last-decision'][contains(.,'Admission ID : " +
+      paNumber +
+      "')]"
+  );
+  await driver.manage().setTimeouts({ implicit: 60000 });
+  const isCaseExists = (await driver.findElements(cuNumberElms).length) !== 0;
+  await driver.manage().setTimeouts({ implicit: 10000 });
+  // scroll to case
+  const el = await driver.findElement(cuNumberElms);
+  await el.getDriver().executeScript("arguments[0].scrollIntoView(true);", el);
+};
 
+const viewDetailCase = async () => {
+  await checkWorkbasket(caNum);
+  let cuNumberElms = By.xpath(
+    "//span[@class='last-decision'][contains(.,'Admission ID : " +
+      paNumber +
+      "')]"
+  );
+  let attempts = 0;
+  while (attempts < 2) {
+    try {
+      await driver.findElement(cuNumberElms).click();
+      waitPageLoadUrl(url);
+      break;
+    } catch (error) {
+      console.log(
+        "case detail page is not showed, trying to click detail case again."
+      );
+    }
+    attempts++;
+  }
+};
 
+module.exports = {
+  viewDetailCase,
+};
